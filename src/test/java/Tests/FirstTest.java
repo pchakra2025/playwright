9aea910abcd456d2a5d71236e4b1ea56cd648d5d
CHAKRAVARTHY POONDRU <prvssc@gmail.com>
2025-11-05 16:07:31 +0530

    single test

--- src/test/java/Tests/FirstTest.java
+++ src/test/java/Tests/FirstTest.java
@@ -1,24 +1,33 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+@Test
 public class FirstTest {
 
-	public static void main(String[] args) {
-//	 Playwright playwright = Playwright.create();
-//	 Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-//	 Page page = browser.newPage();
-//	 page.navigate("https://amdocs.com");
-//	 System.out.println("Hello.. I am " + page.title());
-//	 browser.close();
-		System.out.println("i am running and printing");
-	}
+//	public static void main(String[] args) {
+////	 Playwright playwright = Playwright.create();
+////	 Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+////	 Page page = browser.newPage();
+////	 page.navigate("https://amdocs.com");
+////	 System.out.println("Hello.. I am " + page.title());
+////	 browser.close();
+//		System.out.println("i am running and printing");
+//	}
 	
+	public void verifyTitle() {
+		  Playwright playwright = Playwright.create();
+		  Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+		  Page page = browser.newPage();
+		  page.navigate("https://amdocs.com");
+		  System.out.println("Hello.. I am " + page.title());
+		  browser.close();
+		 	}
 	
-	
-	
-	
+		
 }
